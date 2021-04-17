428dfd29fcb3021eeda7d89606c287b394e55e61
Drew Olsen <olsenj.andrew@gmail.com>
2021-04-17 00:23:08 -0500

    Seeding done

--- models/index.js
+++ models/index.js
@@ -4,34 +4,22 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-//TODO: I don't think any of this is correct. Need to ask questions tomorrow
-
-// Products belongsTo Category
-// Belongs to, use the second model listed, if hasMany its the first model in the phrase
-
 Product.belongsTo(Category, {
-  foreignKey: 'category_id',
+foreignKey: 'category_id'
 });
 
-// Categories have many Products
 Category.hasMany(Product, {
-  through: Category,
-  as: 'related_products',
-  foreignKey: 'category_id',
+foreignKey: 'category_id'
 });
 
-// Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(ProductTag, {
-  through: ProductTag,
-  as: 'related_products',
-  foreignKey: 'tag_id'
+Product.belongsToMany(Tag, {
+through: ProductTag,
+foreignKey: 'product_id',
 });
 
-// Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(ProductTag, {
-  through: Tag,
-  as: 'related_products',
-  foreignKey: 'product_id'
+Tag.belongsToMany(Product, {
+through: ProductTag,
+foreignKey: 'tag_id'
 });
 
 module.exports = {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,14 +4,34 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  
+  try {
+    Category.findAll().then((categoryData) => {
+    res.json(categoryData);
+    include: [{model: Product, Through: Category }]
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
+});
   // find all categories
   // be sure to include its associated Products
-  include: [{model: Product, Through: Category }]
-});
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{ 
+      const categoryData = await Category.findByPk(req.params.id);
+      if(!categoryData) {
+          res.status(404).json({message: 'No category with this id!'});
+          return;
+      }
+      const cat = categoryData.get({ plain: true });
+      res.render('dish', dish);
+    } catch (err) {
+        res.status(500).json(err);
+    };     
 });
 
 router.post('/', (req, res) => {
